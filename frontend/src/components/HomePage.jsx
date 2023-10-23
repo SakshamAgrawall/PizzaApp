53141ddd78e3e3063c90536be4ef9d7dc647672a
saksham <sakshamagrawal18@gmail.com>
2023-10-23 22:08:24 +0530

    change

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -12,23 +12,43 @@ const HomePage = () => {
   useEffect(() => {
     dispatch(getAllPizzas());
   }, [dispatch]);
-  return(
-  <>
-    <Container>
-      {
-      loading ? ( <h1>Loading...</h1>) 
-                : error ? (<h1>error</h1>) 
-                :(
-        <Row>
-          {pizzas.map((pizza) => (
-            <Col md={4} key={pizza._id}>
-              <Pizza pizza={pizza} key={pizza.name} />
-            </Col>
-          ))}
-        </Row>
-      )}
-    </Container>
-  </>
+
+  if (loading) {
+    return (
+      <div
+        className="d-flex justify-content-center align-items-center flex-column"
+        style={{ height: "100vh", backgroundColor: "black", color: "white" }}
+      >
+        <div
+          className="spinner-grow text-white"
+          style={{ width: "3rem", height: "3rem" }}
+          role="status"
+        >
+          <span className="sr-only"></span>
+        </div>
+        <div className="mt-2">
+          Please wait, we are Fatching Latest Pizzas for you 😋
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <Container>
+        {error ? (
+          <h1>error</h1>
+        ) : (
+          <Row>
+            {pizzas.map((pizza) => (
+              <Col md={4} key={pizza._id}>
+                <Pizza pizza={pizza} key={pizza.name} />
+              </Col>
+            ))}
+          </Row>
+        )}
+      </Container>
+    </>
   );
 };
 
